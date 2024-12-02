e1ea76e055203143b903f999bb96d3347d306fde
Makawlly <136134413+Mescaule@users.noreply.github.com>
2024-12-02 13:40:41 -0300

    feat: home page + header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,75 @@
-import Link from "next/link";
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
 
-export default function Header(){
-    return(
-      <>
-    <nav className="flex items-center flex-wrap justify-between bg-white fixed w-full z-10 top-0 py-3">
+export default function Header() {
 
-        <div className="flex items-center top-0">
-        <div className="ml-5">
-			<Link href="/">
-			    <img className="h-12" src="/img/LOGO.png" alt="Logo" />
-			</Link>
-        </div>
+	return (
+		<>
+			<nav className='flex items-center justify-between bg-white relative w-full z-10 py-3 px-5 lg:px-10'>
+				<div className='flex'>
+					<div className='flex items-center'>
+						<Link href='/'>
+							<img className='h-12' src='/img/LOGO.png' alt='Logo' />
+						</Link>
+					</div>
 
-        <div>
-			<ul className="list-reset lg:flex flex-1 items-center">
-				<li className="mr-3">
-					<a className="inline-block text-black text-xl no-underline hover:text-underline py-2 px-4" href="#">RECURSOS</a>
-				</li>
-				<li className="mr-3">
-					<a className="inline-block text-black text-xl no-underline hover:text-underline py-2 px-4" href="#">AIRDROP</a>
-				</li>
-				<li className="mr-3">
-					<a className="inline-block text-black text-xl no-underline hover:text-underline py-2 px-4" href="#">LEADERBOARD</a>
-				</li>
-				<li className="mr-3">
-					<a className="inline-block text-black text-xl no-underline hover:text-underline py-2 px-4" href="#">DRACOIN</a>
-				</li>
-                <li className="mr-3">
-					<a className="inline-block text-black text-xl no-underline hover:text-underline py-2 px-4" href="#">MISSÕES</a>
-				</li>
-			</ul>
-        </div>
-        </div>
-
-        <div className="flex gap-4 mr-10">
-            <Link href="/login"><button className="px-8 border-2 border-primary-blue">ENTRAR</button></Link> 
-            <Link href="/signup"><button className="px-12 bg-primary-blue text-white ">CADASTRAR-SE</button></Link> 
-        </div>
-	</nav>
-      </>
-    )
-  }
+					<ul className='list-reset lg:flex flex-1 items-center text-center lg:text-left'>
+						<li className='mr-0 lg:mr-3'>
+							<a
+								className='inline-block text-black text-lg lg:text-xl no-underline hover:underline py-2 px-4'
+								href='#'
+							>
+								RECURSOS
+							</a>
+						</li>
+						<li className='mr-0 lg:mr-3'>
+							<a
+								className='inline-block text-black text-lg lg:text-xl no-underline hover:underline py-2 px-4'
+								href='#'
+							>
+								AIRDROP
+							</a>
+						</li>
+						<li className='mr-0 lg:mr-3'>
+							<a
+								className='inline-block text-black text-lg lg:text-xl no-underline hover:underline py-2 px-4'
+								href='#'
+							>
+								LEADERBOARD
+							</a>
+						</li>
+						<li className='mr-0 lg:mr-3'>
+							<a
+								className='inline-block text-black text-lg lg:text-xl no-underline hover:underline py-2 px-4'
+								href='#'
+							>
+								DRACOIN
+							</a>
+						</li>
+						<li className='mr-0 lg:mr-3'>
+							<a
+								className='inline-block text-black text-lg lg:text-xl no-underline hover:underline py-2 px-4'
+								href='#'
+							>
+								MISSÕES
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className='flex flex-col lg:flex-row gap-4 px-6 lg:px-0 py-4 lg:py-0'>
+					<Link href='/login'>
+						<button className='px-8 border-2 border-primary-blue'>
+							ENTRAR
+						</button>
+					</Link>
+					<Link href='/signup'>
+						<button className='px-8 bg-primary-blue border-2 border-primary-blue text-white'>
+							CADASTRAR-SE
+						</button>
+					</Link>
+				</div>
+			</nav>
+		</>
+	);
+}
